5f93516e2b0e0aaceafc800e7e0bc3d7a34a0b75
purushoth07 <purushothaman7995@gmail.com>
2020-12-02 08:35:14 +0400

    Add files via upload

---
+++ src/pages/add-horoscope-abroad-travel/add-horoscope-abroad-travel.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AddHoroscopeAbroadTravelPage } from './add-horoscope-abroad-travel';
+
+@NgModule({
+  declarations: [
+    //AddHoroscopeAbroadTravelPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AddHoroscopeAbroadTravelPage),
+  ],
+  exports: [
+    //AddHoroscopeAbroadTravelPage
+  ]
+})
+export class AddHoroscopeAbroadTravelPageModule {}
